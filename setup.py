37c7f75a3d5e211f412d58e950586d3fed3db6d2
nalishamen <nalisha.men@gmail.com>
2021-09-07 17:33:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ie_mbd2021_nmen',
-      version='0.1',  # Development release
+      version='0.1.1',  # Development release
       description='Python Packaging Starting Kit - Simple Structure to create your own package - for training purposes!',
       url='https://github.com/nalishamen/ie_mbd2021_nmen.git',
       author='Nalisha Men',
