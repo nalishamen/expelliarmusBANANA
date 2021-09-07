d3759f607330d3604d296909ce89aed68ed1a070
nalishamen <nalisha.men@gmail.com>
2021-09-07 17:37:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='ie_mbd2021_nmen',
       version='0.1.1',  # Development release
-      description='Python Packaging Starting Kit - Simple Structure to create your own package - for training purposes!',
+      description='Python Package to perform data handling and Risk based approach for a dataset.',
       url='https://github.com/nalishamen/ie_mbd2021_nmen.git',
       author='Nalisha Men',
       author_email='nalisha.m@student.ie.edu',
